6ee747ff2a7d5b613ffc8777294bf1593f0f7f79
default-007 <brianokola@gmail.com>
2020-05-02 12:24:46 +0300

    DEploy to heroku

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
-from flask_script import Manager,Server
+from app import create_app,db
+from flask_script import Manager, Server
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
@@ -12,6 +13,10 @@ def test():
     import unittest
     test = unittest.TestLoader().discover('test')
     unittest.TextTestRunner(verbosity=2).run(test)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 
 if __name__ == '__main__':
     manager.run()
